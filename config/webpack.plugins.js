d9fc3d3781195c3157dd1455f4f8996dfafc3123
Mathias Hörtnagl <m.hoertnagl@web-crossing.com>
2019-10-03 11:07:33 +0200

    bye by air-bnb

--- config/webpack.plugins.js
+++ config/webpack.plugins.js
@@ -5,7 +5,9 @@ const path = require('path');
 const fs = require('fs');
 
 const WebpackBar = require('webpackbar');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const WebappWebpackPlugin = require('webapp-webpack-plugin');
@@ -46,7 +48,10 @@ const robots = new RobotstxtPlugin({
 const clean = new CleanWebpackPlugin();
 
 // Stylelint
-const stylelint = new StyleLintPlugin();
+const stylelint = new StyleLintPlugin({
+  failOnError: false,
+  failOnWarning: false
+});
 
 // Extract CSS
 const cssExtract = new MiniCssExtractPlugin({
@@ -108,7 +113,9 @@ const webpackBar = new WebpackBar({
 const CODE = `<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create','{{ID}}','auto');ga('send','pageview');</script>`;
 
 class GoogleAnalyticsPlugin {
-  constructor({ id }) {
+  constructor({
+    id
+  }) {
     this.id = id;
   }
 
